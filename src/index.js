b889c4d5db80cab721042d3c0576b55c4d1dede9
Jesus Aguiar <j.adolfo.3@gmail.com>
2023-09-01 11:12:44 -0400

    go

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+// import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import 'tachyons';
-import App from './containers/App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import App from './containers/App';
+import { searchRobots, requestRobots } from './reducers';
+
+import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+const root = createRoot(document.getElementById("root"));
 
 root.render(
   <>
     <React.StrictMode>
-      <App/>
+      <Provider store={store}>
+        <App/>
+      </Provider>
     </React.StrictMode >
   </>
 )
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
